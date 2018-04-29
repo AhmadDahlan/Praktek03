25da70de414f1cda6d7224dc59d13881c89ecafd
AhmadDahlan <ahmddhln197@gmail.com>
2018-04-29 18:23:23 +0700

    Instance buat atrribut dan memanggil method

--- src/praktek03/BajuAksi.java
+++ src/praktek03/BajuAksi.java
@@ -2,7 +2,12 @@ package praktek03;
 
 public class BajuAksi {
     public static void main(String[] args) {
-        
+       Baju b = new Baju();
+       b.merk="Billabong";
+       b.warna="Merah";
+       b.harga=25000;
+       
+       b.pelindungtubuh();
     }
     
 }
