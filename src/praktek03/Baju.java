8e3b55b78613555f330211d685e869bc70be7769
AhmadDahlan <ahmddhln197@gmail.com>
2018-04-29 18:16:41 +0700

    Buat Method PelindungTubuh()

--- src/praktek03/Baju.java
+++ src/praktek03/Baju.java
@@ -4,4 +4,10 @@ public class Baju {
     String merk;
     String warna;
     double harga;
+    
+    void pelindungtubuh(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga+"\n");
+    }
 }
