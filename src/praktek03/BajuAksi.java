d8050d99e1a9ec4f8ac99f1a42c6cfe78c32939d
AhmadDahlan <ahmddhln197@gmail.com>
2018-04-29 18:32:19 +0700

    Menggunakan method set

--- src/praktek03/BajuAksi.java
+++ src/praktek03/BajuAksi.java
@@ -3,9 +3,9 @@ package praktek03;
 public class BajuAksi {
     public static void main(String[] args) {
        Baju b = new Baju();
-       b.merk="Billabong";
-       b.warna="Merah";
-       b.harga=25000;
+       b.setMerk("Billabongss");
+       b.setWarna("Hijau");
+       b.setHarga(25000);
        
        b.pelindungtubuh();
     }
